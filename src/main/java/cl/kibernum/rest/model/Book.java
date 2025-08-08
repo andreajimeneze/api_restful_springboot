6131507a09fd24094303178084c0c7a9cc3bccc8
Andrea Jiménez <andreacjimenez@gmail.com>
2025-08-07 21:26:36 -0400

    modelo terminado

---
+++ src/main/java/cl/kibernum/rest/model/Book.java
@@ -0,0 +1,36 @@
+package cl.kibernum.rest.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Book {
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+
+     public Long getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+
+    
+}
